0e34b35a63bb232222b0acca7ac950a0fda970f0
ninid <ninid@NINIS-VICTUS>
2024-02-25 23:08:22 +0400

    Add project files.

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,55 @@
+﻿using GuessNumber;
+
+// Generating a random number between 1 and 100
+int number = new Random().Next(1,100);
+
+// Declaring variable
+string input;
+int inputNumber=0,count=0;
+
+// Prompting the user to guess the numbe
+Console.WriteLine("X is between 1 and 100. Guess X.");
+
+while (true && inputNumber != number)
+{
+    try
+    {
+        // Reading user input
+        input = Console.ReadLine();
+
+        // Validating input
+        if (!int.TryParse(input, out inputNumber))
+        {
+            throw new WrongInputException();
+        }
+        else
+        {
+            // Parsing user input to an integer
+            inputNumber = int.Parse(input);
+
+            // Comparing user input with the generated number
+            if (inputNumber > number)
+            {
+                Console.WriteLine($"X is less than {inputNumber}");
+                count++;
+            }
+            else if(inputNumber < number)
+            {
+                Console.WriteLine($"X is grater than {inputNumber}");
+                count++;
+            }
+            else
+            {
+                // Counting user's guesses
+                count++;
+
+                // Displaying a congratulatory message if the user guesses the number correctly
+                Console.WriteLine($"Congratulations. You have guessed X. \nX = {number} \nattempts : {count}");
+            }
+        }
+    }
+    catch (WrongInputException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
---
+++ GuessNumber/WrongInputException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    internal class WrongInputException : ApplicationException
+    {
+        private string _msg;
+
+        public WrongInputException()
+        {
+            _msg = "Input must be integer number. Try again.";
+        }
+        public override string Message
+        {
+            get { return $"{_msg}"; }
+        }
+    }
+}
